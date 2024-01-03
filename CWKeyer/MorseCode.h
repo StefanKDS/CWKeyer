8a19d5646933a08103a584d83f28f2ef84c3e9a4
Stefan <stefseyer@gmail.com>
2024-01-03 20:06:58 +0100

    Update MorseCode.h

--- CWKeyer/MorseCode.h
+++ CWKeyer/MorseCode.h
@@ -30,6 +30,35 @@ char* MORSE_LETTERS[] = {
   "--.."    // Z
 };
 
+String LETTERS[] = {
+  "A",     // A
+  "B",   // B
+  "C",   // C
+  "D",    // D
+  "E",      // E
+  "F",   // F
+  "G",    // G
+  "H",   // H
+  "I",     // I
+  "J",   // J
+  "K",    // K
+  "L",   // L
+  "M",     // M
+  "N",     // N
+  "O",    // O
+  "P",   // P
+  "Q",   // Q
+  "R",    // R
+  "S",    // S
+  "T",      // T
+  "U",    // U
+  "V",   // V
+  "W",    // W
+  "X",   // X
+  "Y",   // Y
+  "Z"    // Z
+};
+
 char* MORSE_NUMBERS[] = {
   "-----",   // 0
   ".----",   // 1
